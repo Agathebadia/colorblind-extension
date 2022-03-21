609e5f1a5383ea9de15c4db4627ecb91e7f799dd
Florian Kraft <schnuffifk@gmail.com>
2022-03-21 20:49:28 +0100

    updates to new Chrome API

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
-chrome.scripting.executeScript({
-    target: {tabId: chrome.window.getCurrent, allFrames: true},
-    files: ['colorblindListener.js'],
-});
+chrome.action.onClicked.addListener(execScript);
+
+async function execScript() {
+  const tabId = await getTabId();
+  chrome.scripting.executeScript({
+    target: { tabId: chrome.window.getCurrent, allFrames: true },
+    files: ["colorblindListener.js"],
+  });
+}
+
+async function getTabId() {
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  return tabs.length > 0 ? tabs[0].id : null;
+}
--- colorblindListener.js
+++ colorblindListener.js
@@ -2,9 +2,9 @@
 //Injects a javascript file on click event, the js file applies a filter to simulate colorblindness
 
 window.selectedFilter = null;
-window.onload = function() {
+window.onload = function () {
   if (!chrome || !chrome.storage || !chrome.storage.local) return;
-  chrome.storage.local.get(["key"], function(result) {
+  chrome.storage.local.get(["key"], function (result) {
     try {
       document.getElementById(result.key).click();
     } catch (e) {
@@ -15,22 +15,32 @@ window.onload = function() {
 
 function setSelected(value) {
   try {
-    chrome.storage.local.set({ key: value }, function() {
+    chrome.storage.local.set({ key: value }, function () {
       document.getElementById(value).checked = true;
     });
   } catch {}
 }
 
-function injectFilter(fileName) {
-  chrome.tabs.executeScript({ file: fileName });
+function injectFilter(fileName, tabId) {
+  chrome.scripting.executeScript({
+    files: [fileName],
+    target: { tabId, allFrames: true },
+  });
 }
 
-document.querySelectorAll(['[id^="radio"]']).forEach(radioButton => {
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+document.querySelectorAll(['[id^="radio"]']).forEach((radioButton) => {
   const filter = radioButton.parentElement.id.replace("option-", "");
-  radioButton.addEventListener("click", function() {
+  radioButton.addEventListener("click", async function () {
+    const tab = await getCurrentTab();
     // page-specific filters
     setSelected(radioButton.id);
-    injectFilter(`filters/${filter}.js`);
+    injectFilter(`filters/${filter}.js`, tab.id);
     // popup-specific filters
     applyFilter((window.selectedFilter = filter));
   });
